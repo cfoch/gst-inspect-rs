a8016bc1a6c10440fee6b8e7f40bb23c5b55f936
Fabian Orccon <cfoch.fabian@gmail.com>
2023-05-28 01:53:18 +0200

    inspect: elements: List plugin, feature, element long name

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,40 @@
 //
 // You should have received a copy of the GNU Lesser General Public
 // License along with this program; if not, see <http://www.gnu.org/licenses/>.
+extern crate gstreamer as gst;
+
+use crate::gst::prelude::Cast;
+use crate::gst::prelude::GstObjectExt;
+use ansi_term::Color;
+
+const BRBLUE: Color = Color::RGB(97, 127, 166);
+const PLUGIN_NAME_COLOR: Color = BRBLUE;
+const ELEMENT_NAME_COLOR: Color = Color::Green;
+
+fn print_element_list() {
+    let registry = gst::Registry::get();
+    let mut plugins = registry.plugins();
+
+    plugins.sort_by(|p1, p2| p1.plugin_name().as_str().cmp(p2.plugin_name().as_str()));
+    for plugin in &plugins {
+        let mut features = registry.features_by_plugin(&plugin.plugin_name());
+
+        features.sort_by(|f1, f2| f1.name().as_str().cmp(f2.name().as_str()));
+        for feature in &features {
+            if let Some(element_factory) = feature.downcast_ref::<gst::ElementFactory>() {
+                println!(
+                    "{}:  {}: {}",
+                    PLUGIN_NAME_COLOR.paint(plugin.plugin_name().to_string()),
+                    ELEMENT_NAME_COLOR.paint(element_factory.name().to_string()),
+                    element_factory.longname()
+                );
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    gst::init().unwrap();
+
+    print_element_list();
 }
